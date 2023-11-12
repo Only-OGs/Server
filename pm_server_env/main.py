db5ddf1bbbe198aad9fd37a8acc26696c086bfa4
Pascal378 <pascal.schwartz@protonmail.com>
2023-11-12 18:24:58 +0100

    add json object handling and venv

---
+++ pm_server_env/main.py
@@ -0,0 +1,39 @@
+import socketio
+import eventlet
+
+# Server erstellen
+sio = socketio.Server(async_handlers=True,cors_allowed_origins='*')
+
+# WSGI App
+app = socketio.WSGIApp(sio)
+
+# Verbindung eines neuen Clients
+@sio.event
+def connect(sid, environ):
+    print(f"Client connected: {sid}")
+
+# Verbindungsabbruch eines Clients
+@sio.event
+def disconnect(sid):
+    print(f"Client disconnected: {sid}")
+
+# Event Handler für eingehende Nachrichten
+@sio.event
+def message(sid, data):
+    try:
+        # Hier gehen wir davon aus, dass der empfangene Datenstring ein JSON-Objekt ist
+        json_data = sio.manager.rooms[sid].decode(data)
+
+        # Jetzt können Sie das JSON-Objekt verarbeiten
+        print(f"Received JSON from {sid}: {json_data}")
+
+        # Beispiel: Senden Sie eine Antwort an den Client
+        response_data = {'status': 'success', 'message': 'JSON received successfully'}
+        sio.emit('response', response_data, room=sid)
+
+    except Exception as e:
+        print(f"Error processing JSON from {sid}: {e}")
+
+if __name__ == '__main__':
+    # Starten mit eventlet
+    eventlet.wsgi.server(eventlet.listen(('localhost', 8080)), app)
