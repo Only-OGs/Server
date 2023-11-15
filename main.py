514694cf8807a5119d179cefad697ff1b871596a
Pascal378 <pascal.schwartz@protonmail.com>
2023-11-15 21:46:54 +0100

    Add some documentation, fix formatting

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ sio = socketio.Server(async_handlers=True, cors_allowed_origins='*')
 # WSGI App
 app = socketio.WSGIApp(sio)
 
+# Clients die aktuell connected sind, Value ist True, wenn diese nur connected sind,
+# sind sie tatsächlich eingeloggt haben sie einen Username
 connected_clients = {}
+
+# User die aktuell registriert sind
 users = {}
 
+# Pfad zum
 file_path = "users.txt"
 
 
@@ -42,6 +47,8 @@ def message(sid, data):
     except Exception as e:
         print(f"Error processing JSON from {sid}: {e}, {data}")
 
+
+# Login, prüft Benutzernamen auf Existenz und in solchem Fall auch auf korrektes Passwort
 @sio.event
 def login(sid, data):
     name = data["user"]
@@ -51,13 +58,15 @@ def login(sid, data):
         if users[name] == password:
             connected_clients[sid] = name
             response_data = {'status': 'login_success', 'message': f"Login erfolgreich, willkommen {name}"}
-            sio.emit('response', response_data, room=sid)
         else:
             response_data = {'status': 'login_failed', 'message': "Passwort nicht korrekt"}
-            sio.emit('response', response_data, room=sid)
     else:
         response_data = {'status': 'login_failed', 'message': f"{name} ist nicht registriert."}
-        sio.emit('response', response_data, room=sid)
+
+    sio.emit('response', response_data, room=sid)
+
+
+# Registrierung, prüft ob User bereits existiert, sollte dem nicht so sein wird er angelegt
 @sio.event
 def register(sid, data):
     name = data["user"]
@@ -65,26 +74,32 @@ def register(sid, data):
 
     if name in users:
         response_data = {'status': 'register_failed', 'message': f"{name} ist bereits registriert"}
-        sio.emit('response', response_data, room=sid)
     else:
         write_user_to_file(data, sid)
         response_data = {'status': 'register_success', 'message': f"{name} wurde erfolgreich registriert"}
-        sio.emit('response', response_data, room=sid)
+
+    sio.emit('response', response_data, room=sid)
+
+
+# User die registriert werden, werden in die users.txt geschrieben und anschließend zur
+# Laufzeit in das User dict aufgenommen
 def write_user_to_file(data, sid):
     name = data["user"]
     password = data["password"]
 
     try:
         with open(file_path, 'a') as file:
-                file.write(f"{name} {password}\n")
+            file.write(f"{name} {password}\n")
 
         print(f"Die Benutzerdaten wurden erfolgreich in die Datei {file_path} geschrieben.")
         users[name] = password
 
     except Exception as e:
         print(f"Ein Fehler ist aufgetreten: {e}")
-def load_registered_users():
 
+
+# Lädt registrierte User aus users.txt in das User dict
+def load_registered_users():
     try:
         with open(file_path, 'r') as file:
             for line in file:
@@ -99,7 +114,9 @@ def load_registered_users():
         print(f"Ein Fehler ist aufgetreten: {e}")
         return None
 
+
 if __name__ == '__main__':
-    # Starten mit eventlet
+    # User laden
     load_registered_users()
+    # Starten mit eventlet
     eventlet.wsgi.server(eventlet.listen(('localhost', 8080)), app)
