5df8af0332748b9149054c675deb9e0fa0f8b3ab
Pascal378 <pascal.schwartz@protonmail.com>
2023-12-12 17:07:58 +0100

    Fix small issue where a lobby with only one ready client would start

--- Entities/Lobby.py
+++ Entities/Lobby.py
@@ -99,11 +99,13 @@ class Lobby:
         self.timer_started = True
 
         while counter != -2:
-            if not self.check_all_ready() and self.connections > 1:
+            if (not self.check_all_ready() and self.connections > 1) or self.connections == 1:
                 events.sio.emit("timer_abrupt", "Timer has been abrupt", room=self.id)
                 print("timer_abrupt sent to ", self.id)
                 self.timer_started = False
                 return
+
+
 
             print(self.id, " counter is ", counter)
             eventlet.sleep(1)
